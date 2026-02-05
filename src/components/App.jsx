0bdf0b158b5e5b87f27745254b5b0c17ea34f0ac
Vamshi <vamshivamsharaj123@gmail.com>
2026-02-05 23:11:54 +0530

    Add CreateArea component to create new notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+ 
+  const [notes, setNotes] = useState([]);
+
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  // const [items, setItems] = useState([]);
+
+  // function addItem(inputText) {
+  //   setItems(prevItems => {
+  //     return [...prevItems, inputText];
+  //   });
+  // }
+
+
   return (
     <div>
       <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem,index)=>{
+        return (
+          <Note
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+onDelete={deleteNote}
+          />
+        )
+      })}
       
-  {notes.map(note=>(
-    <Note
-    key={note.id}
-    title={note.title}
-    content={note.content}
-    />
-  ))
-}
-
       <Footer />
     </div>
   );
